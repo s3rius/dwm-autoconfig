ca467adff1b23945cad413779e4bcd809021aa7e
Pavel Kirilin <win10@list.ru>
2020-08-03 01:16:05 +0400

    Updated window parameters.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -26,10 +26,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class            instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",        NULL,       NULL,       1 << 8,       0,           -1 },
-  { "Avizo-service",  NULL,       NULL,       0,            1,           -1 }
+	/* class            instance    title       tags mask     iscentered    isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       0,            1,            1,           -1 },
+	{ "Firefox",        NULL,       NULL,       1 << 8,       1,            0,           -1 },
+  { "Avizo-service",  NULL,       NULL,       0,            1,            1,           -1 }
 };
 
 /* layout(s) */
