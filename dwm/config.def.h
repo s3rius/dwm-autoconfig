f9e36d14c3a54643d425d287dc9f2cd955d73a6d
Pavel Kirilin <win10@list.ru>
2020-08-04 19:04:07 +0400

    Updated icons.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "5", "6", "7", "", "" };
+static const char *tags[] = { "", "", "", "", "5", "", "7", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
